ba8c14594614034982f389253e7e5d1b4ca34e54
duyongchang <735383226@qq.com>
2019-04-23 17:30:41 +0800

    ssss

--- app/src/main/java/testgit/dyc/com/testgit/MainActivity.kt
+++ app/src/main/java/testgit/dyc/com/testgit/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
-        sample_text.append("cached")
+        sample_text.append("reposity2")
     }
 
     /**
