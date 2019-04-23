68bafcece17cd867f777ca4c1dde5194f1778c8d
duyongchang <735383226@qq.com>
2019-04-23 15:26:30 +0800

    ssss

--- app/src/main/java/testgit/dyc/com/testgit/MainActivity.kt
+++ app/src/main/java/testgit/dyc/com/testgit/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
-        sample_text.append("sdfsfsdfsdfsdf")
+        sample_text.append("ssssssssssssssssssssssssssssssssssssss")
     }
 
     /**
